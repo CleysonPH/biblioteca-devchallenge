85ae50b259e9eab92ed3359ce7a63d3d346e98a3
Cleyson Lima <cleysonph@gmail.com>
2021-04-05 03:25:28 -0300

    add Book update feature

--- src/main/java/com/cleysonph/booksapi/controller/BookController.java
+++ src/main/java/com/cleysonph/booksapi/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -56,6 +57,23 @@ public class BookController {
 
         book.setId(id);
         return bookRepository.save(book);
+    }
+
+    @PatchMapping("/{id}")
+    public Book update(@RequestBody Book book, @PathVariable Long id) {
+        Book bookToUpdate = getById(id);
+
+        if (book.getTitle() != null) {
+            bookToUpdate.setTitle(book.getTitle());
+        }
+        if (book.getPublisher() != null) {
+            bookToUpdate.setPublisher(book.getPublisher());
+        }
+        if (book.getPhoto() != null) {
+            bookToUpdate.setPhoto(book.getPhoto());
+        }
+
+        return bookRepository.save(bookToUpdate);
     }
 
 }
