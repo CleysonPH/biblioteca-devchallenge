fdcb21c6673184b950002bea6d2b0dc56ec68cd2
Cleyson Lima <cleysonph@gmail.com>
2021-04-05 03:04:44 -0300

    add Book replace feature

--- src/main/java/com/cleysonph/booksapi/controller/BookController.java
+++ src/main/java/com/cleysonph/booksapi/controller/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -47,6 +48,14 @@ public class BookController {
         Book book = getById(id);
 
         bookRepository.delete(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book replace(@RequestBody Book book, @PathVariable Long id) {
+        getById(id);
+
+        book.setId(id);
+        return bookRepository.save(book);
     }
 
 }
